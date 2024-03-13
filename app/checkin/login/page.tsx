fbda0e521d5aff6a52983fb4d1309d897f261a5b
supreeya1707 <supreeya1707@gmail.com>
2024-03-13 23:00:39 +0700

    13/03 22.44

--- app/checkin/login/page.tsx
+++ app/checkin/login/page.tsx
@@ -4,13 +4,33 @@ import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import { setInterval } from "timers";
+import { Input } from "@/components/ui/input";
 // import Clock from "./clock";
 // import Clock from "react-live-clock";
 
 function page() {
   return (
-    <div className="containner w-full ">
-      <div className=" bg-indigo-500"></div>
+    <div className="containner  ">
+      <div className=" grid grid-cols-auto  justify-self-center ">
+        <Image
+          className="flex justify-items-center self-start "
+          src="/image/PerfectDay_head 1.png"
+          alt={""}
+          width={390}
+          height={214}
+        ></Image>
+      </div>
+      <div className=" bgblue grid grid grid-flow-row auto-rows-max justify-self-center content-start  ">
+        <div className="mt-5 w-[239px]  h-[41px]  ">
+          <Input type="username" placeholder="username" />
+        </div>
+        <div className="mt-5 w-[239px]  h-[41px] grid grid-cols-auto  justify-self-center ">
+          <Input type="password" placeholder="password " />
+        </div>
+        <div className="mt-5 w-[175px]  h-[41px] grid grid-cols-auto  justify-self-center ">
+          <Button className="border-2 bg-[#F26B22] border-white">Login</Button>
+        </div>
+      </div>
     </div>
   );
 }
--- app/checkin/page.tsx
+++ app/checkin/page.tsx
@@ -6,8 +6,51 @@ import { Button } from "@/components/ui/button";
 import { setInterval } from "timers";
 // import Clock from "./clock";
 // import Clock from "react-live-clock";
+import liff from "@line/liff"
+import axios from "axios";
 
 function page() {
+  const [loading,setloading]=useState("");
+  
+  useEffect(() => {
+
+    const initLiff = async () => {
+      setloading(false);
+      await liff.init({ liffId: lineliffid }).then(async () => {
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          const profile = await liff.getProfile()
+          console.log(profile);
+          setProfile(profile)
+          setLineId(profile?.userId);
+          setloading(true);
+          console.warn(lineId);
+          form.reset({
+            username: '',
+            password: '',
+            line_id: profile?.userId
+          });
+
+          const checkLineId = await axios.get(`https://hyggemedicalservice.com/apirbh/rbh-line-numalee/lineid/${profile.userId}`);
+          if (checkLineId.data.length > 0) {
+            setCheckuser(true);
+            setUser(checkLineId.data[0]);
+          } else {
+            setCheckuser(false);
+          }
+        }
+      });
+      await liff.ready
+    }
+
+    try {
+      initLiff()
+    } catch (e: any) {
+      console.error('liff init error', e.message)
+    }
+
+  }, [lineId])
   return (
     <div className="containner ">
       <h1>TSERT</h1>
